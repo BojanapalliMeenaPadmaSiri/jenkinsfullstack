4ee5fbfcce884967cbd38621f8978e7dea024d68
Siri <padmasiri2805@gmail.com>
2025-09-05 23:53:30 +0530

    Updated backend to WAR packaging and SpringBootServletInitializer

--- Backend/ems-backend/src/main/java/com/example/ems/EmsBackendApplication.java
+++ Backend/ems-backend/src/main/java/com/example/ems/EmsBackendApplication.java
@@ -2,12 +2,12 @@ package com.example.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;  // <-- add this
 
 @SpringBootApplication
-public class EmsBackendApplication {
+public class EmsBackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmsBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmsBackendApplication.class, args);
+    }
 }
